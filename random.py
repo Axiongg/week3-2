37235b601ebe5fecf3e234034ac96bd654e23922
Axiongg <161007911+Axiongg@users.noreply.github.com>
2024-03-14 09:39:28 +0800

    Add files via upload

---
+++ random.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 09:04:16 2024
+
+@author: student
+"""
+
+import random 
+
+n = int(input("n: "))
+a = int(input("a: "))
+b = int(input("b: "))
+
+random_choose_list = []
+
+while len(random_choose_list) < n:
+    x = random.randint(a, b)
+    random_choose_list.append(x)
+    
+random_choose_list = set(random_choose_list)
+random_choose_list = list(random_choose_list)
+random_choose_list.sort()
+random_choose_list.reverse()
+
+print(random_choose_list)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
